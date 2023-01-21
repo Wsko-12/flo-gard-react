28e5699a0c0a91b89feb85bf03849068bef79db7
Wsko-12 <vaskovladislav@gmail.com>
2023-01-21 15:08:44 +0300

    fix: clone ground and grass material

--- src/game/Materials/GlassMaterial.ts
+++ src/game/Materials/GlassMaterial.ts
@@ -1,23 +1,19 @@
-import { DoubleSide, MeshPhongMaterial } from 'three';
+import { DoubleSide } from 'three';
 import { EColorsPallet } from '../world/environment/utils/utils';
 import { PhongMaterialWithCloseCameraShader } from './PhongWithCloseCamera';
 
-let saved: MeshPhongMaterial | null = null;
-
 export const getGlassMaterial = (closeCameraDistance: number | undefined = undefined) => {
-  if (!saved) {
-    saved = PhongMaterialWithCloseCameraShader(
-      {
-        opacity: 0.25,
-        transparent: true,
-        shininess: 100,
-        side: DoubleSide,
-      },
-      closeCameraDistance
-    );
-    saved.userData.staticColor = EColorsPallet.glass;
-    saved.depthWrite = false;
-  }
+  const material = PhongMaterialWithCloseCameraShader(
+    {
+      opacity: 0.25,
+      transparent: true,
+      shininess: 100,
+      side: DoubleSide,
+    },
+    closeCameraDistance
+  );
+  material.userData.staticColor = EColorsPallet.glass;
+  material.depthWrite = false;
 
-  return saved;
+  return material;
 };
